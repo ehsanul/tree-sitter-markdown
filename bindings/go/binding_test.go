ff71bc74660c76d37cddc96695956f7a9a0d4064
Ehsanul Hoque <ehsanul@ehsanul.com>
2026-02-06 22:12:58 -0800

    fix binding_test

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,7 +3,7 @@ package tree_sitter_markdown_test
 import (
 	"testing"
 
-	tree_sitter_markdown "github.com/tree-sitter-grammars/tree-sitter-markdown/tree-sitter-markdown/bindings/go"
+	tree_sitter_markdown "github.com/tree-sitter-grammars/tree-sitter-markdown/bindings/go"
 	tree_sitter "github.com/tree-sitter/go-tree-sitter"
 )
 
